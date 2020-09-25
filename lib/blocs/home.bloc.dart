2108b4a094f5f96e68142b7e0c46f6337b6755fc
Cleiton Ribeiro <cleiton.r@globomail.com>
2020-09-24 22:01:07 -0300

    Provider and State manager

--- lib/blocs/home.bloc.dart
+++ lib/blocs/home.bloc.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/widgets.dart';
 import 'package:shopping/repositories/category.repository.dart';
 
 import '../models/category-list-item.model.dart';
 import '../models/product-list-item.model.dart';
 import '../repositories/product.repository.dart';
 
-class HomeBloc {
+class HomeBloc extends ChangeNotifier{
   final categoryRepository = new CategoryRepository();
   final productRepository = new ProductRepository();
 
@@ -21,18 +22,21 @@ class HomeBloc {
   getCategories() {
     categoryRepository.getAll().then((data) {
       this.categories = data;
+      notifyListeners();
     });
   }
 
   getProducts() {
     productRepository.getAll().then((data) {
       this.products = data;
+      notifyListeners();
     });
   }
 
   getProductsByCategory() {
     productRepository.getByCategory(selectedCategory).then((data) {
       this.products = data;
+      notifyListeners();
     });
   }
 
