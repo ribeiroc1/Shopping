f6a16ff1e35dc7080ea712595686848b6797fb57
Cleiton Ribeiro <cleiton.r@globomail.com>
2020-09-28 16:11:06 -0300

    Add/remove carrinho

---
+++ lib/ui/shared/widgets/shared/add-to-cart.widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping/blocs/cart.bloc.dart';
+import 'package:shopping/models/cart-item.model.dart';
+import 'package:shopping/models/product-list-item.model.dart';
+
+class AddToCart extends StatelessWidget {
+  final ProductListItemModel item;
+
+  AddToCart({@required this.item});
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of<CartBloc>(context);
+
+    var cartItem = new CartItemModel(
+      id: item.id,
+      price: item.price,
+      image: item.image,
+      quantity: 1,
+      title: item.title,
+    );
+
+    if (!bloc.itemInCar(cartItem)) {
+      return Container(
+        width: 80,
+        height: 40,
+        child: FlatButton(
+          color: Theme.of(context).primaryColor,
+          child: Icon(Icons.add_shopping_cart),
+          textColor: Colors.white,
+          onPressed: () {
+            bloc.add(
+              cartItem,
+            );
+            final snackBar =
+                SnackBar(content: Text('${item.title} adicionado'));
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+        ),
+      );
+    } else {
+      return Container(
+        width: 80,
+        height: 40,
+        child: FlatButton(
+          color: Theme.of(context).primaryColor,
+          child: Icon(Icons.remove_shopping_cart),
+          textColor: Colors.red,
+          onPressed: () {
+            bloc.remove(
+              cartItem,
+            );
+            final snackBar =
+                SnackBar(content: Text('${item.title} removido'));
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+        ),
+      );
+    }
+  }
+}
