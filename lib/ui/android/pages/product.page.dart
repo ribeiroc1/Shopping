2a27392f52307cf977553b4b0833cc9a9d37ef5c
Cleiton Ribeiro <cleiton.r@globomail.com>
2020-09-28 08:11:54 -0300

    Products Page - Card

---
+++ lib/ui/android/pages/product.page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/models/product-details.model.dart';
+import 'package:shopping/repositories/product.repository.dart';
+import 'package:shopping/ui/shared/widgets/shared/progress-indicator.widget.dart';
+
+class ProductPage extends StatelessWidget {
+  final String tag;
+  final _repository = new ProductRepository();
+
+  ProductPage({@required this.tag});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ProductDetailsModel>(
+      future: _repository.get(tag),
+      builder: (context, snapshot) {
+        ProductDetailsModel product = snapshot.data;
+
+        if (snapshot.data == null) {
+          return Center(
+            child: Container(
+              child: Text("Nenhum produto encontrado"),
+            ),
+          );
+        } else {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text('Aguardando...');
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Center(
+                child: GenericProgressIndicator(),
+              );
+            case ConnectionState.done:
+              if (snapshot.hasError)
+                return Center(
+                  child: Container(
+                    child: Text(snapshot.error),
+                  ),
+                );
+              return content(product);
+          }
+        }
+      },
+    );
+  }
+
+  Widget content(ProductDetailsModel product) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: new ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: product.images.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          return Container(
+            width: 200,
+            child: Image.network(
+              product.images[index],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
