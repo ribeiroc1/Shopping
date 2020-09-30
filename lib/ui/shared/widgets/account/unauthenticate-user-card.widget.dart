1af310ad3893966c43f6e9003953fcea454a1c19
Cleiton Ribeiro <cleiton.r@globomail.com>
2020-09-29 22:08:05 -0300

    Widgest de login autenticado/não autenticado

---
+++ lib/ui/shared/widgets/account/unauthenticate-user-card.widget.dart
@@ -0,0 +1,41 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:shopping/ui/android/pages/login.page.dart';
+import 'package:shopping/ui/android/pages/signup.page.dart';
+
+class UnauthenticateUserCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        SizedBox(
+          height: 60,
+        ),
+        FlatButton(
+          child: Text("Autentique-se"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => LoginPage(),
+              ),
+            );
+          },
+        ),
+        FlatButton(
+          child: Text("Ainda não sou cadastrado"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignupPage(),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
